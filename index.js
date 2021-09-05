e286acb41957d8cde3e8c9cb22775b9dbceb88c5
Denys <denysburimov@gmail.com>
2021-09-05 21:34:17 +0200

    Desktop tabs in process

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+var swiper = new Swiper(".mySwiper", {
+        loop: true,
+
+        slidesPerView: "auto",
+        centeredSlides: false,
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+
 $(document).ready(function() {
     $(".mobile_menu_button").click(function() {
         alert("Click happend!");
+    });
+
+    $(".tab").click(function() {
+      $(".tab").css({'color': '#ababab'});
+      $(this).css({'color': '#4b94c6'});
+      
     });
 });
---
+++ swiper.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+        loop: true,
+
+        slidesPerView: "auto",
+        centeredSlides: false,
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
